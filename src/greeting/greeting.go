c26c626dede54f5a2d3830783ca7c0337c410778
platylm <n_nareenart@kkumail.com>
2018-07-28 20:33:04 +0700

    [Edited]:function GreetingMessage

--- src/greeting/greeting.go
+++ src/greeting/greeting.go
@@ -1,8 +1,13 @@
 package greeting
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Messeage struct {
@@ -32,4 +37,19 @@ func HelloPost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	encoder := json.NewEncoder(w)
 	encoder.Encode(messeage)
+}
+
+func GreetingMessage(name string) string {
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/bootcamp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var prefix string
+	err = db.QueryRow("SELECT message FROM greeting  WHERE id = ?", 1).Scan(&prefix)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fmt.Sprintf("%s %s", prefix, name)
 }
