e864f72377e47a86ff462163ab25129c839f4ae3
platylm <n_nareenart@kkumail.com>
2018-07-28 14:31:21 +0700

    [Added]:function hello type post

--- src/greeting/greeting.go
+++ src/greeting/greeting.go
@@ -1,11 +1,35 @@
 package greeting
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+type Messeage struct {
+	Txt string `json:"txt"`
+}
+
+type User struct {
+	Name string `json:"name"`
+}
+
 func Hello(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, `{"txt::"hello world"}`)
+	values := r.URL.Query()
+	name := values.Get("name")
+	messeage := Messeage{Txt: "hello " + name}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(messeage)
+}
+
+func HelloPost(w http.ResponseWriter, r *http.Request) {
+	var u User
+	body := json.NewDecoder(r.Body)
+	body.Decode(&u)
+	messeage := Messeage{Txt: "hello " + u.Name}
+
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(messeage)
 }
