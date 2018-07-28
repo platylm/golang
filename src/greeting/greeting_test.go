77c8c94f57a6ecddf4ee67bf2bc4b32327bd3adb
platylm <n_nareenart@kkumail.com>
2018-07-28 14:31:00 +0700

    [Edited]:add function test type post

--- src/greeting/greeting_test.go
+++ src/greeting/greeting_test.go
@@ -5,11 +5,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
-func Test_Hello_Func(t *testing.T) {
-	expected := `{"txt::"hello world"}`
+func Test_Hello_Get_Func(t *testing.T) {
+	expected := `{"txt":"hello world"}`
 	r, _ := http.NewRequest(http.MethodGet, "/hello?name=world", nil)
 	w := httptest.NewRecorder()
 
@@ -17,9 +18,26 @@ func Test_Hello_Func(t *testing.T) {
 
 	response := w.Result()
 	body, _ := ioutil.ReadAll(response.Body)
-	actual := string(body)
+	actual := strings.TrimSpace(string(body))
 
 	if actual != expected {
-		t.Errorf("expected %s but got %s", expected, actual)
+		t.Errorf("expected '%s' but got '%s'", expected, actual)
+	}
+}
+
+func Test_Hello_Post_Func(t *testing.T) {
+	expected := `{"txt":"hello world"}`
+	msg := `{"name":"world"}`
+	r, _ := http.NewRequest(http.MethodPost, "/hello-post", strings.NewReader(msg))
+	w := httptest.NewRecorder()
+
+	HelloPost(w, r)
+
+	response := w.Result()
+	body, _ := ioutil.ReadAll(response.Body)
+	actual := strings.TrimSpace(string(body))
+
+	if actual != expected {
+		t.Errorf("expected '%s' but got '%s'", expected, actual)
 	}
 }
